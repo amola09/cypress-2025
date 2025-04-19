a1355da9e1afa26f68788650dd4f3d4ec28ffb03
amola09 <amol7anpat@gmail.com>
2025-04-19 15:17:37 +0530

    second commit

---
+++ cypress/e2e/3-Basics/TC-01.contactUs.cy.js
@@ -0,0 +1,14 @@
+///<reference types = "cypress" />
+
+describe("TC-01: Verify ContactUs Page from Webdriver university:",function(){
+    it("TC001-Verify ContactUS page for valid data:",function(){
+
+        //AAA - Arrangement,Action,Assertion
+        // Arrangement
+        cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        // Action
+        cy.get()
+        // Assertion
+
+    })
+})
